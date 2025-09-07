efd9c9d06adfefbab2cdeb5da5587cf677327497
Jamova01 <jorgemova01@gmail.com>
2025-09-07 15:34:24 -0500

    feat(components): add molecule components (FormTextField, FormTextareaField)

---
+++ frontend/src/components/molecules/FormTextField.tsx
@@ -0,0 +1,49 @@
+import { Control, FieldPath, FieldValues } from "react-hook-form";
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../atoms/Form";
+import { Input } from "../atoms/input";
+
+interface FormTextFieldProps<T extends FieldValues>
+  extends React.ComponentProps<"input"> {
+  name: FieldPath<T>;
+  label: string;
+  placeholder: string;
+  control: Control<T>;
+  type?: string;
+}
+
+export const FormTextField = <T extends FieldValues>({
+  name,
+  label,
+  placeholder,
+  control,
+  type = "text",
+  ...inputProps
+}: FormTextFieldProps<T>) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              type={type}
+              {...field}
+              {...inputProps}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
---
+++ frontend/src/components/molecules/FormTextareaField.tsx
@@ -0,0 +1,41 @@
+import { Control, FieldPath, FieldValues } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../atoms/Form";
+import { Textarea } from "../atoms/textarea";
+
+interface FormTextareaFieldProps<T extends FieldValues>
+  extends React.ComponentProps<"textarea"> {
+  name: FieldPath<T>;
+  label: string;
+  placeholder?: string;
+  control: Control<T>;
+}
+
+export const FormTextareaField = <T extends FieldValues>({
+  name,
+  label,
+  placeholder,
+  control,
+  ...textareaProps
+}: FormTextareaFieldProps<T>) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Textarea placeholder={placeholder} {...field} {...textareaProps} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
