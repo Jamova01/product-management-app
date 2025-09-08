8fc1913cb962bec12c0c6c68b38f17aa2e535acb
Jamova01 <jorgemova01@gmail.com>
2025-09-08 11:45:43 -0500

    Add Zod schema for product validation

---
+++ frontend/src/schemas/product.schema.ts
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+export const ProductSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "El nombre debe tener al menos 2 caracteres" }),
+  description: z
+    .string()
+    .min(5, { message: "La descripción debe tener al menos 5 caracteres" }),
+  price: z
+    .number({ error: "El precio debe ser un número" })
+    .positive("El precio debe ser mayor a 0"),
+  imageUrl: z.url("Debe ser una URL válida").optional().or(z.literal("")),
+});
+
+export type ProductFormData = z.infer<typeof ProductSchema>;
