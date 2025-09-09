8b53e98e3e3dd424a3361a3251601b91d4a466ad
Jamova01 <jorgemova01@gmail.com>
2025-09-08 20:22:52 -0500

    feat: implement transformProduct helper in ProductsService

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,42 +1,58 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PrismaService } from 'src/database/prisma.service';
+
+function transformProduct(
+  product: Product,
+): Omit<Product, 'price'> & { price: number } {
+  return {
+    ...product,
+    price: Number(product.price),
+  };
+}
 
 @Injectable()
 export class ProductsService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createProductDto: CreateProductDto) {
-    return this.prisma.product.create({
+    const product = await this.prisma.product.create({
       data: createProductDto,
     });
+    return transformProduct(product);
   }
 
   async findAll() {
-    return this.prisma.product.findMany();
+    const products = await this.prisma.product.findMany();
+    return products.map(transformProduct);
   }
 
   async findOne(id: string) {
-    const product = await this.prisma.product.findUnique({ where: { id } });
+    const product = await this.prisma.product.findUnique({
+      where: { id },
+    });
     if (!product) {
       throw new NotFoundException(`Product with ID ${id} not found`);
     }
-    return product;
+    return transformProduct(product);
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
     await this.findOne(id);
-
-    return this.prisma.product.update({
+    const product = await this.prisma.product.update({
       where: { id },
       data: updateProductDto,
     });
+    return transformProduct(product);
   }
 
   async remove(id: string) {
     await this.findOne(id);
-
-    return this.prisma.product.delete({ where: { id } });
+    const product = await this.prisma.product.delete({
+      where: { id },
+    });
+    return transformProduct(product);
   }
 }
